f69ea57871c2be7bbb8d7f6a68d40e5080380638
Gabrielly Assuncao <gabriellyassuncaorodrigues@gmail.com>
2023-10-16 22:26:03 -0300

    limpando código

--- startrek/src/carrosel.jsx
+++ startrek/src/carrosel.jsx
@@ -1,149 +1,4 @@
-// import Slider from 'react-slick';
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
-// import React, { useState, useEffect } from 'react';
-// import TypingEffect from './functionTest';
-// import dialogo from './dialogo.json';
-// import './style/carousel.css';
 
-// const Carousel = () => {
-//   const graph = {
-//     Coridan: { RuraPenthe: 10, QuVat: 15, Vulcan: 20 },
-//     RuraPenthe: { Vulcan: 10, Yaraka: 12, Trill: 10 },
-//     Vulcan: { QuVat: 8, Trill: 20, RuraPenthe: 10 },
-//     QuVat: { Yaraka: 8, Regulus: 11, Trill: 15 },
-//     Yaraka: { Suliban: 10, Trill: 8, RuraPenthe: 12, QuVat: 8 },
-//     Suliban: { Orion: 10, Regulus: 9, Yaraka: 10 },
-//     Qonos: { Regulus: 8, Sona: 6, Yaraka: 8 },
-//     Regulus: { Orion: 7, Qonos: 6, Suliban: 9, Qonos: 8 },
-//     Orion: { Sona: 5, Qonos: 7, Regulus: 7, Suliban: 10 },
-//     Sona: { Qonos: 5, Orion: 5, Regulus: 6 },
-//     Qonos: { Sona: 5, Orion: 7, Regulus: 6 }
-//   };
-
-
-//   const startNode = "Vulcan";
-//   const endNode = "Qonos";
-
-//   const shortestPath = dijkstra(graph, startNode, endNode);
-
-//   console.log("Menor caminho de", startNode, "para", endNode, ":", shortestPath.join(" -> "));
-
-
-
-//   const [dialog, setDialog] = useState([]);
-//   const [currentCharacter, setCurrentCharacter] = useState(0);
-//   const [isOnPage2, setIsOnPage2] = useState(false);
-//   const [startTyping, setStartTyping] = useState(false);
-
-//   useEffect(() => {
-//     // Carregue o diálogo do seu arquivo JSON
-//     setDialog(dialogo.items);
-//   }, []);
-
-//   const settings = {
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     arrows: true,
-//     beforeChange: (current, next) => {
-//       if (next === 1) {
-//         setIsOnPage2(true);
-//         setTimeout(() => {
-//           setStartTyping(true);
-//         }, 1000);
-//       }
-//     },
-//   };
-//   function dijkstra(graph, startNode, endNode) {
-//     const distances = {};
-//     const previousNodes = {};
-//     const visitedNodes = [];
-
-//     for (let node in graph) {
-//       distances[node] = Infinity;
-//     }
-
-//     distances[startNode] = 0;
-
-//     while (visitedNodes.length < Object.keys(graph).length) {
-//       const unvisitedNodes = Object.keys(graph).filter(node => !visitedNodes.includes(node));
-//       const currentNode = unvisitedNodes.reduce((minNode, node) => {
-//         return distances[node] < distances[minNode] ? node : minNode;
-//       }, unvisitedNodes[0]);
-
-//       const neighbors = graph[currentNode];
-
-//       for (let neighbor in neighbors) {
-//         const tentativeDistance = distances[currentNode] + neighbors[neighbor];
-//         if (tentativeDistance < distances[neighbor]) {
-//           distances[neighbor] = tentativeDistance;
-//           previousNodes[neighbor] = currentNode;
-//         }
-//       }
-
-//       visitedNodes.push(currentNode);
-
-//       if (currentNode === endNode) {
-//         break;
-//       }
-//     }
-
-//     if (!previousNodes[endNode]) {
-//       return [];
-//     }
-
-//     const path = [];
-//     let currentNode = endNode;
-//     while (currentNode !== startNode) {
-//       path.unshift(currentNode);
-//       currentNode = previousNodes[currentNode];
-//     }
-//     path.unshift(startNode);
-
-//     return path;
-//   }
-
-//   return (
-//     <div className='carousel'>
-//       <Slider {...settings} className='slider'>
-//         <div>
-//           <h2>Bem vindo à Jornada Estelar</h2>
-//         </div>
-//         <div>
-//           {isOnPage2 && startTyping && (
-//             <div className='teste'>
-//               {dialog[currentCharacter] && (
-//                 <div className='dialog'>
-//                   <strong>{dialog[currentCharacter].personagem}:</strong>
-//                   <TypingEffect text={dialog[currentCharacter].falas[0]} />
-//                 </div>
-//               )}
-//               <button className='next'
-//                 onClick={() => {
-//                   if (currentCharacter < dialog.length - 1) {
-//                     setCurrentCharacter(currentCharacter + 1);
-//                   }
-//                 }}
-//               >
-//                avançar
-//               </button>
-//             </div>
-//           )}
-//         </div>
-//         <div>
-//           <h1>Resultado do algoritmo</h1>
-//         </div>
-//       </Slider>
-//     </div>
-//   );
-// };
-
-// export default Carousel;
-
-import galaxy from './assets/galaxy.mp4';
 import React, { useState, useEffect } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -164,20 +19,16 @@ const Carousel = () => {
     QuVat: { Yaraka: 8, Regulus: 11, Trill: 15 },
     Yaraka: { Suliban: 10, Trill: 8, RuraPenthe: 12, QuVat: 8 },
     Suliban: { Orion: 10, Regulus: 9, Yaraka: 10 },
-    Qonos: { Regulus: 8, Sona: 6, Yaraka: 8 },
-    Regulus: { Orion: 7, Qonos: 6, Suliban: 9, Qonos: 8 },
+    Trill: { Regulus: 8, Sona: 6, Yaraka: 8 },
+    Regulus: { Orion: 7, Qonos: 6, Suliban: 9, Trill: 8 },
     Orion: { Sona: 5, Qonos: 7, Regulus: 7, Suliban: 10 },
     Sona: { Qonos: 5, Orion: 5, Regulus: 6 },
     Qonos: { Sona: 5, Orion: 7, Regulus: 6 }
   };
 
-  // const startNode = "Vulcan";
-  // const endNode = "Qonos";
   const [startPlanet, setStartPlanet] = useState("Vulcan");
   const [endPlanet, setEndPlanet] = useState("Qonos");
   const [shortestPath, setShortestPath] = useState([]);
-
-  // const shortestPath = dijkstra(graph, startNode, endNode);
 
   const [dialog, setDialog] = useState([]);
   const [currentCharacter, setCurrentCharacter] = useState(0);
@@ -283,7 +134,6 @@ const Carousel = () => {
           )}
         </div>
         <div>
-          {/* <h1>Resultado do algoritmo</h1> */}
             <h2>Escolha os planetas de início e destino</h2>
           <div className='options'>
             <label>Planeta de Início:</label>
--- startrek/src/sidebar.jsx
+++ startrek/src/sidebar.jsx
@@ -1,33 +1,3 @@
-// import React, { useState } from 'react';
-// import './style/sidebar.css';
-
-// function Sidebar() {
-//   const [collapsed, setCollapsed] = useState(false);
-
-//   const toggleSidebar = () => {
-//     setCollapsed(!collapsed);
-//   };
-
-//   return (
-//     <div className={`sidebar ${collapsed ? 'collapsed' : ''}`}>
-//       <button className="toggle-button" onClick={toggleSidebar}>
-//         Toggle Sid
-//       </button>
-//       <div>
-
-//       <ul>
-//         <li>Item 1</li>
-//         <li>Item 2</li>
-//         <li>Item 3</li>
-//       </ul>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Sidebar;
-
-
 import React, { useState } from 'react';
 import './style/sidebar.css';
 
@@ -47,7 +17,7 @@ function Sidebar() {
         <ul>
       <h1>MAPAS DA GALAXIA</h1>
           <li><a href='https://cdn.obsidianportal.com/map_images/363685/spaceView1.jpg'>Mapa Geral</a></li>
-          <li><a href='https://www.ussventure.eng.br/LCARS-Terminal_net_arquivos/nave/NewMaps/klingon2.jpg'>Empério Klingon</a></li>
+          <li><a href='https://www.ussventure.eng.br/LCARS-Terminal_net_arquivos/nave/NewMaps/klingon2.jpg'>Império Klingon</a></li>
           <li><a href='https://www.ussventure.eng.br/LCARS-Terminal_net_arquivos/nave/NewMaps/ufp03.jpg'>Federação</a></li>
         </ul>
       </div>
